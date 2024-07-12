720d3011ddaab671248850238cf015bedff00990
NguyenDuc45 <nguyenduc04052002@gmail.com>
2024-07-12 11:08:35 +0700

    Test

--- index.php
+++ index.php
@@ -1,41 +1,40 @@
 <?php
-    // Require files trong commons
-    require_once "./commons/env.php";
-    require_once "./commons/helper.php";
-    require_once "./commons/connect-db.php";
+// Require files trong commons
+require_once "./commons/env.php";
+require_once "./commons/helper.php";
+require_once "./commons/connect-db.php";
 
-    // Require files trong controllers và models
-    require_file(PATH_CONTROLLER);
-    require_file(PATH_MODEL);
+// Require files trong controllers và models
+require_file(PATH_CONTROLLER);
+require_file(PATH_MODEL);
 
-    include PATH_VIEW . "header.php";
+include PATH_VIEW . "header.php";
 
-    // Điều hướng
-    $act = $_GET["act"] ?? "/";
-    $a = match ($act) {
-        "/" => homeIndex(),
-        
-        "danhmuc" => danhmucList(),
-        "danhmuc_add" => danhmucAdd(),
-        "danhmuc_update" => danhmucUpdate(),
-        "danhmuc_delete" => danhmucDelete(),
+// Điều hướng
+$act = $_GET["act"] ?? "/";
+$a = match ($act) {
+    "/" => homeIndex(),
 
-        "sanpham" => sanphamList(),
-        "sanpham_add" => sanphamAdd(),
-        "sanpham_update" => sanphamUpdate(),
-        "sanpham_delete" => sanphamDelete(),
+    "danhmuc" => danhmucList(),
+    "danhmuc_add" => danhmucAdd(),
+    "danhmuc_update" => danhmucUpdate(),
+    "danhmuc_delete" => danhmucDelete(),
 
-        "taikhoan" => taikhoanList(),
-        "taikhoan_add" => taikhoanAdd(),
-        "taikhoan_update" => taikhoanUpdate(),
-        "taikhoan_delete" => taikhoanDelete(),
+    "sanpham" => sanphamList(),
+    "sanpham_add" => sanphamAdd(),
+    "sanpham_update" => sanphamUpdate(),
+    "sanpham_delete" => sanphamDelete(),
 
-        "binhluan" => binhluanList(),
-        
-        "donhang" => donhangList(),
-    };
-    
-    include PATH_VIEW . "footer.php";
-    
-    require_once "./commons/disconnect-db.php";
-?>
+    "taikhoan" => taikhoanList(),
+    "taikhoan_add" => taikhoanAdd(),
+    "taikhoan_update" => taikhoanUpdate(),
+    "taikhoan_delete" => taikhoanDelete(),
+
+    "binhluan" => binhluanList(),
+
+    "donhang" => donhangList(),
+};
+
+include PATH_VIEW . "footer.php";
+
+require_once "./commons/disconnect-db.php";
